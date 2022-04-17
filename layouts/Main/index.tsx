ff90be3d98c8f45ad90aa57550c1b68ee133f07c
unknown <coa2769@gmail.com>
2022-04-17 22:26:16 +0900

    Main Layout 구현 (03) Tab 완료

--- layouts/Main/index.tsx
+++ layouts/Main/index.tsx
@@ -1,15 +1,10 @@
 import React, { useState, useCallback } from "react";
-import { Link, Route, Switch, RouteComponentProps } from "react-router-dom";
-import { Header, PageContainer, tap_style, MenuContainer, RowContainer } from '@layouts/Main/style'
+import { Route, Switch, RouteComponentProps } from "react-router-dom";
+import { Header, PageContainer, tap_style, menu_style, MenuContainer, RowContainer } from '@layouts/Main/style'
 
 import TabsUnstyled from '@mui/base/TabsUnstyled';
 import TabsListUnstyled from '@mui/base/TabsListUnstyled';
 import TabUnstyled from '@mui/base/TabUnstyled';
-
-
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
-import BookIcon from '@mui/icons-material/Book';
 
 import Cog from '@layouts/Cog';
 import Asset from '@layouts/Asset';
@@ -25,29 +20,27 @@ const Main = (props : RouteComponentProps)=>{
             props.history.push(`/main/${value}`);
         }, [currentTab]);
 
+    // const onSelectTab = useCallback(
+    //     (event : React.SyntheticEvent<Element, Event>)=>{
+    //         const parent = (event.target as HTMLElement).parentElement;
+    //         console.log((parent as HTMLElement).children);
+    //         // const children : Array<HTMLElement> = 
+    //         // console.log((event.target as HTMLInputElement).style.borderLeftColor = '#FF6E40');
+    //         // console.log((event));
+    //     }, []);
+
     return(
         <RowContainer>
             <MenuContainer>
                 <Header>
-                    <BookIcon />
-                    <h1>Menu</h1>
+                    <h1>MENU</h1>
                 </Header>
-                <TabsUnstyled orientation='vertical' defaultValue={currentTab}>
-                    <TabsListUnstyled >
-                        <TabUnstyled onChange={onChangeTab} value="cog">장부</TabUnstyled>
-                        <TabUnstyled onChange={onChangeTab} value="asset">자산</TabUnstyled>
+                <TabsUnstyled defaultValue={"cog"}>
+                    <TabsListUnstyled style={menu_style} component={"ul"}>
+                        <TabUnstyled onChange={onChangeTab} value="cog" style={tap_style} component={"li"}>장부</TabUnstyled>
+                        <TabUnstyled onChange={onChangeTab} value="asset" style={tap_style} component={"li"}>자산</TabUnstyled>
                     </TabsListUnstyled>
                 </TabsUnstyled>
-                {/* <Tabs
-                    orientation="vertical"
-                    value={currentTab}
-                    onChange={onChangeTab}
-                    aria-label="Vertical tabs example"
-                    sx={{ borderRight: 1, borderColor: 'divider' }}
-                    >
-                    <Tab label="장부" value="cog" style={tap_style} component={Link} to="/main/cog"/>
-                    <Tab label="자산" value="asset" style={tap_style} component={Link} to="/main/asset"/>
-                </Tabs> */}
             </MenuContainer>
             <PageContainer>
                 <Switch>
@@ -55,53 +48,6 @@ const Main = (props : RouteComponentProps)=>{
                     <Route path="/main/asset" component={Asset} />
                 </Switch>    
             </PageContainer>
-        {/* Header */}
-
-        {/* <img src={strip} alt="" /> */}
-        {/* <Box sx={{ width: '100%', bgcolor: 'background.paper' }}> */}
-        {/* <TabsUnstyled defaultValue={"cog"} background-image={strip}> */}
-        {/* <TabsListUnstyled style={tap_list_style} > */}
-            {/* <TabUnstyled value="cog" style={tap_style} component={Link} to="/main/cog">장부</TabUnstyled> */}
-            {/* <TabUnstyled value="asset" style={tap_style} component={Link} to="/main/asset">자산</TabUnstyled> */}
-        {/* </TabsListUnstyled> */}
-        {/* <TabPanelUnstyled value={"cog"}>First page</TabPanelUnstyled> */}
-        {/* <TabPanelUnstyled value={"asset"}>Second page</TabPanelUnstyled> */}
-        {/* <TabPanelUnstyled value={2}>Third page</TabPanelUnstyled> */}
-        {/* </TabsUnstyled> */}
-        {/* </Box> */}
-
-
-        {/* value={currentTab} onChange={onChangeTab} style={taps_style} */}
-        {/* <Box sx={{ width: '100%', bgcolor: 'background.paper' }}> */}
-            {/* <Tabs value={currentTab} onChange={onChangeTab} > */}
-                {/* <TabUnstyled>장부</TabUnstyled> */}
-                {/* <TabUnstyled>자산</TabUnstyled> */}
-                {/* <Tab label="장부" value="cog" style={tap_style} component={Link} to="/main/cog"/> */}
-                {/* <Tab label="자산" value="asset" style={tap_style} component={Link} to="/main/asset"/> */}
-            {/* </Tabs> */}
-        {/* </Box> */}
-        {/* 장부 or 자산 */}
-
-        {/* <Box display="flex" flex={1} flexDirection="column"> */}
-            {/* <Header> 
-                <h1>가계부</h1>
-            </Header>
-            <Tabs
-                orientation="vertical"
-                variant="scrollable"
-                value={currentTab}
-                onChange={onChangeTab}
-                aria-label="Vertical tabs example"
-                sx={{ borderRight: 1, borderColor: 'divider' }}
-            >
-                <Tab label="장부" value="cog" style={tap_style} component={Link} to="/main/cog"/>
-                <Tab label="자산" value="asset" style={tap_style} component={Link} to="/main/asset"/>
-            </Tabs>
-            <Switch>
-                <Route path="/main/cog" component={Cog} />
-                <Route path="/main/asset" component={Asset} />
-            </Switch> */}
-        {/* </Box> */}
         </RowContainer>
   
     )
--- layouts/Main/style.tsx
+++ layouts/Main/style.tsx
@@ -11,6 +11,15 @@ export const MenuContainer = styled.nav`
     height:100%;
     width:200px;
     background-color:#ECEFF1;
+    border-right: 1px solid #CFD8DC;
+
+    li{
+        border-left: 4px solid transparent;
+    }
+
+    li.Mui-selected {
+        border-left: 4px solid #FF6E40;
+    }
 `;
 
 export const PageContainer = styled.main`
@@ -21,21 +30,43 @@ export const PageContainer = styled.main`
 export const Header = styled.header`
     width : 100%;
     height : 60px;
-    padding-left : 24px;
+    
     display:flex;
     align-items: center;
-    justify-content: left;
+    justify-content: center;
     color: #455A64;
     h1 {
         margin : 0;
-        padding-left : 10px;
+        letter-spacing: 2px;
     }
 `;
 
+export const menu_style = {
+    margin : '0',
+    padding : '0',
+    height : 'calc(100% - 60px)',
+    listStyle : 'none',
+};
 
 
 export const tap_style = {
     color: "#455A64",
-    height : "28px",
+    height : "49px",
+    cursor : 'pointer',
+    fontSize : '14px',
+    fontWeight : 500,
+    lineHeight : '3.5',
+    textAlign : 'center' as const,
     
-}
+};
+
+//padding-left : 24px;
+//'padding-left' : '60px',
+//padding-left : 10px;
+
+/*
+'border-left' : '4px solid transparent',
+    li:focus {
+        border-left: 4px solid #FF6E40;
+    }
+*/
